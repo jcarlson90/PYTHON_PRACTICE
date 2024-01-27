d692c0e4617084659f1263798147b563532fb13b
jcarlson <jake.carlson90@gmail.com>
2024-01-27 12:18:07 -0800

    practice

---
+++ python.py
@@ -0,0 +1,45 @@
+MY_CONSTANT  = 5
+# constants do not exist in python, convention is to name in all caps
+my_variable = "Hello World"
+# all variable by default are re-assignable, snake case is the norm in python
+
+#print varibale to the console with the print function
+print(MY_CONSTANT)
+print(my_variable)
+
+########## if statements ##########
+num = 5
+
+if (num > 3):
+    print("num is greate than 3")
+elif (num > 1):
+    print("num is greater than 1")
+else: 
+    print("num is 1 or less")
+
+# indentation is required in python
+    
+num = 10
+
+if (num > 9):
+    print("num is greater than 9")
+elif (num < 20):
+    print("num is less than 20")
+else:
+    print("num is 9 or less")
+
+##
+
+x = 3
+y = 10
+
+if x < y:
+    print("y is greater than x")
+
+##
+    
+
+
+
+
+
